d744e35256f63a735e9435bdf13070a283361002
Narongchai <narongchai@MSI>
2025-04-25 02:50:13 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Page = () => {
+    return(
+        <>
+            <h1>This is about page</h1>
+        </>
+    )
+}
+export default Page;
---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = ({params} : {params : {id : string}}) => {
+
+  const { id } = params;
+  return (
+    <h1>User Profile : {id}</h1>
+  )
+}
+
+export default page
